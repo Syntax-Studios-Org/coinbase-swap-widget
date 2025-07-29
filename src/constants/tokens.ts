f4b6684e9387d4695a9c3426108e650e3cdead42
Suhel <suhelkapadia2@gmail.com>
2025-07-29 20:20:42 +0530

    feat: token icons

--- src/constants/tokens.ts
+++ src/constants/tokens.ts
@@ -6,28 +6,28 @@ export const BASE_TOKENS: Record<string, Token> = {
     symbol: "ETH",
     name: "Ethereum",
     decimals: 18,
-    logoUrl: "https://cryptologos.cc/logos/ethereum-eth-logo.png",
+    logoUrl: "/icons/eth.svg",
   },
   USDC: {
     address: "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913",
     symbol: "USDC",
     name: "USD Coin",
     decimals: 6,
-    logoUrl: "https://cryptologos.cc/logos/usd-coin-usdc-logo.png",
+    logoUrl: "/icons/usdc.svg",
   },
   WETH: {
     address: "0x4200000000000000000000000000000000000006",
     symbol: "WETH",
     name: "Wrapped Ether",
     decimals: 18,
-    logoUrl: "https://cryptologos.cc/logos/ethereum-eth-logo.png",
+    logoUrl: "/icons/weth.svg",
   },
   DAI: {
     address: "0x50c5725949A6F0c72E6C4a641F24049A917DB0Cb",
     symbol: "DAI",
     name: "Dai Stablecoin",
     decimals: 18,
-    logoUrl: "https://cryptologos.cc/logos/multi-collateral-dai-dai-logo.png",
+    logoUrl: "/icons/dai.svg",
   },
 } as const;
 
